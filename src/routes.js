7966c0db74ec71f36dba8f9125a66673e8da11a3
João Fernandes <joaofergear003@gmail.com>
2023-01-30 16:10:09 -0300

    feat: create initial shape of routes

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+exports.routes = [
+  {
+    method: 'GET',
+    endpoint: '/users',
+    runController: null,
+  },
+  {
+    method: 'GET',
+    endpoint: '/users/:id',
+    runController: null,
+  },
+  {
+    method: 'POST',
+    endpoint: '/users',
+    runController: null,
+  },
+  {
+    method: 'PUT',
+    endpoint: '/users',
+    runController: null,
+  },
+  {
+    method: 'DELETE',
+    endpoint: '/users',
+    runController: null,
+  },
+]
