d2a03b265f06a553d7c12f3c4f8511562d4b3cac
João Fernandes <joaofergear003@gmail.com>
2023-02-01 13:23:38 -0300

    refactor: apply the injectSendHelper() & change the routes conditional

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ const http = require('http')
 const { routes } = require('./routes')
 const { parseBody } = require('./helpers/parse-body')
 const { getMatchedRoute } = require('./helpers/get-matched-route')
+const { injectSendHelper } = require('./helpers/inject-send-helper')
 
 const server = http.createServer((req, res) => {
   const { isRouteExists, matchedRoute } = getMatchedRoute(req)
   
-  if (isRouteExists) {
-    if (['POST', 'PUT'].includes(req.method)) {
-      parseBody(req, () => matchedRoute.runController(req, res))
-      return
-    }
-    matchedRoute.runController(req, res)
+  injectSendHelper(res)
+  
+  if (!isRouteExists) {
+    res.send(404, `Cannot ${req.method} at ${req.url}`, 'text/html')
+    return
   }
   
-  res.writeHead(404, { 'Content-Type': 'text/html' })
-  res.end(`Cannot ${req.method} at ${req.url}`)
+  if (['POST', 'PUT'].includes(req.method)) {
+    parseBody(req, () => matchedRoute.runController(req, res))
+    return
+  }
+  
+  matchedRoute.runController(req, res)
 })
 
 server.listen(3000, () => console.log('Server has been started at http://localhost:3000'))
